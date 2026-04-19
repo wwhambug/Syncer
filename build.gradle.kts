033180813d48c4511acef866b47739c8b1fa4856
wwhambug <wwhambug@gmail.com>
2026-04-19 23:00:59 +0900

    build.gradle.kts 만들기

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// build.gradle.kts (Project: echo-sync-extension)
+
+plugins {
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+}
